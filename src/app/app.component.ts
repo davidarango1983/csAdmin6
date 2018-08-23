df481005a1c423a596676b3cb0d44d9b975a0535
davidarango <soldaditorockero@hotmail.com>
2018-08-23 23:22:21 +0200

    Origin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'csAdmin6';
+  name = 'csAdmin6';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
+import {ServerComponent} from "./server/server.component";
+
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent} from './home/home.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {Routes, RouterModule} from '@angular/router';
+import {HeaderComponent} from "./header/header.component";
+import {FooterComponent} from "./footer/footer.component";
+import {ResumeTableComponent} from "./resume-table/resume-table.component";
+import {Cons} from "./providers/cons";
+
+
+const appRoutes: Routes = [
+  {path: 'home', component: HomeComponent}
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    ResumeTableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes)
+
   ],
-  providers: [],
+  providers: [Cons],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
