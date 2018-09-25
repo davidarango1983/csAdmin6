ca86b954ba92ee2c3c0e6f2054da2b566c5aa560
davidarango <soldaditorockero@hotmail.com>
2018-09-25 23:36:25 +0200

    Añadido contador y estilos

--- src/app/resume-table/resume-table.component.ts
+++ src/app/resume-table/resume-table.component.ts
@@ -3,27 +3,25 @@ import { NgxSpinnerService } from 'ngx-spinner';
 import { HttpCustomClient } from '../services/httpCustom.service';
 import { Url } from '../constants/url';
 import { isNullOrUndefined } from 'util';
+import { of, Observable, interval, Subscription } from 'rxjs';
+import { map } from 'rxjs/operators'
 
-
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
 
 
 @Component({
   selector: 'app-resume-table',
   templateUrl: './resume-table.component.html',
-  styleUrls: ['./resume-table.component.scss']
-})
+  styleUrls: ['./resume-table.component.scss'],
+ })
 export class ResumeTableComponent implements OnInit {
 
 
   constructor(private spinner: NgxSpinnerService, private http: HttpCustomClient,private url: Url) { }
   resume: Resume[];
+  state = 'normal';
+  total: Subscription;
+  conteo = 0;
+  finished: boolean = false;
 
   ngOnInit() {
 
@@ -37,11 +35,41 @@ export class ResumeTableComponent implements OnInit {
       console.log("An error has ocurred while loading resume");
     });
 
-
-    
   
+  }
+
+
+ngAfterViewInit(){
+  
+  const myNumbers = interval(1)
+  .pipe(map(
+    (data: number) => {
+      return data + 1;
+    }
+  ));
+this.total = myNumbers.subscribe(
+  (number: number) => {
+    console.log(number);
+    this.conteo=number;
+    if(this.conteo==200){
+      this.total.unsubscribe();
+      this.finished=true;
+      
+    }
+  }
+
+  
+);
+
+
+
 }
+
+
+ 
+
 }
+
 
 export interface Resume {
 
